1f3639e864a23fb6bc5f89ffb5c8efb5ff4a9c72
SemicolonManifest <theo.gautier@cpnv.ch>
2023-01-23 12:48:51 +0100

    update routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,9 +5,9 @@ const azureBlobController = require("./api/dataObject/azureBlob");
 
 module.exports = async (app) => {
   app.route("/hello").get(hellocontroller.hello);
-  app.route("/containers/:containerName/blobs/all").get(await azureBlobController.all);
   app.route("/containers").get(await azureBlobController.containers);
-  app.route("/containers/:containerName/blobs/exists/:blobName").get(await azureBlobController.exists);
-  app.route("/containers/:containerName/blobs/create/:blobName/:content").post(await azureBlobController.create);
-  app.route("/containers/:containerName/blobs/delete/:blobName").delete(await azureBlobController.delete);
+  app.route("/containers/:containerName/blobs/all").get(await azureBlobController.all);
+  app.route("/containers/:containerName/blobs/:blobName/exists").get(await azureBlobController.exists);
+  app.route("/containers/:containerName/blobs/:blobName/:content").post(await azureBlobController.create);
+  app.route("/containers/:containerName/blobs/:blobName").delete(await azureBlobController.delete);
 };
